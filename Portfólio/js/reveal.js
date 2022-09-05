a3410b5591df9b1ab0544c03f45279ad819e3200
Baku Wall-e <wysthellyjessemata13@gmail.com>
2022-09-05 14:37:24 -0300

    JavaScript

---
+++ Portfólio/js/reveal.js
@@ -0,0 +1,14 @@
+var slideUp = {
+    delay: 500,
+    duration: 1000,
+    reset: true,
+    opacity: 0
+}
+
+ScrollReveal().reveal('.title-1', slideUp);
+
+ScrollReveal().reveal('.title-2', slideUp);
+
+ScrollReveal().reveal('.title-3', slideUp);
+
+ScrollReveal().reveal('.title-4', slideUp);
