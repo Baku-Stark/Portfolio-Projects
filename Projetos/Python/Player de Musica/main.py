5fcfe345216f1d430f44ba660d32159d3240b7e2
Baku Wall-e <wysthellyjessemata13@gmail.com>
2022-12-31 10:54:16 -0300

    🐍 Update: Music Player v2.0

--- Projetos/Python/Player de Musica/main.py
+++ Projetos/Python/Player de Musica/main.py
@@ -8,6 +8,12 @@ from tkinter import messagebox
 from PIL import Image, ImageTk
 # IMPORTAÇÃO [pygame]
 from pygame import mixer
+#  ======================================================
+# IMPORTAÇÃO [rich > para denúncia de erros]
+from rich import print
+from rich.console import group
+from rich.panel import Panel
+
 
 
 #  ======================================================
@@ -41,14 +47,14 @@ class Music():
     
     def prevMusic(self):
         '''
-            Tocar música ANTERIOR
-
-            Vars:
-                playing_music: Música atual
-                index_list: Index do arquivo de música na pasta 'song'
-                new_index_list: Novo index (retonar o valor do arquivo)
-                self.arq_music[-1]: Retorna o último valor da pasta 'song'
-        '''
+            Tocar música ANTERIOR.
+            |
+            |
+            `--> playing_music : Música atual.
+            `--> index_list : Index do arquivo de música na pasta 'song'.
+            `--> new_index_list : Novo index (retonar o valor do arquivo).
+            `--> self.arq_music[-1] : Retorna o último valor da pasta 'song'.
+	    '''
 
         try:
             self.diretorio_atual = os.getcwd()
@@ -60,6 +66,7 @@ class Music():
             self.set_music['text'] = self.arq_music[new_index_list]
 
             mixer.music.load(rf"{self.diretorio_atual}\song\{self.set_music['text']}")
+            
             mixer.music.play()
 
         except IndexError:
@@ -72,8 +79,12 @@ class Music():
 
     def playMusic(self):
         '''
-            Tocar a música selecionada
-        '''
+            Tocar a música selecionada.
+            |
+            |
+            `--> Lista de música
+	    '''
+
         self.set_music['text'] = self.listMusic.get(ACTIVE)
         self.set_music['anchor'] = 'w'
         mixer.music.load(rf"{self.diretorio_atual}\song\{self.listMusic.get(ACTIVE)}")
@@ -81,14 +92,14 @@ class Music():
         
     def nextMusic(self):
         '''
-            Tocar música SEGUINTE
-
-            Vars:
-                playing_music: Música atual
-                index_list: Index do arquivo de música na pasta 'song'
-                new_index_list: Novo index (retonar o valor do arquivo)
-                self.arq_music[0]: Retorna o último valor da pasta 'song'
-        '''
+            Tocar música SEGUINTE.
+            |
+            |
+            `--> playing_music : Música atual
+            `--> index_list : Index do arquivo de música na pasta 'song'
+            `--> new_index_list : Novo index (retonar o valor do arquivo)
+            `--> self.arq_music[0] : Retorna o último valor da pasta 'song'
+	    '''
 
         try:
             self.diretorio_atual = os.getcwd()
@@ -100,6 +111,7 @@ class Music():
             self.set_music['text'] = self.arq_music[new_index_list]
 
             mixer.music.load(rf"{self.diretorio_atual}\song\{self.set_music['text']}")
+            
             mixer.music.play()
 
         except IndexError:
@@ -113,9 +125,12 @@ class Music():
 class App(Music):
     def __init__(self):
         '''
-            Função principal para a
-            exibição da janela.
-        '''
+            Função principal para a exibição da janela.
+            |
+            |
+            `--> functions
+	    '''
+
         self.root = Tk()
         self.screen()
         self.frameScreen()
@@ -128,19 +143,16 @@ class App(Music):
 
     def screen(self):
         '''
-            Configuração principal da aplicação
+            Configuração principal da aplicação.\n
+            |\n
+            |\n
+            `--> title      : Título da aplicação.\n
+            `--> geometry   : Dimensão da janela (width x height).\n
+            `--> resizable  : Ajustar tamanho da página (opção negada)\n
+            `--> configure [background] : Cor de fundo\n
+            `--> iconbitmap : Ícone da janela\n
+	    '''
 
-            Return:
-                title: Título da aplicação
-
-                geometry: Dimensão da janela (width x height)
-
-                resizable: Ajustar tamanho da página (opção negada)
-
-                configure [background]: Cor de fundo
-
-                iconbitmap: Ícone da janela
-        '''
         self.root.title("Music Player 2.0")
         self.root.geometry("450x300")
         self.root.resizable(width='false', height='false')
@@ -149,24 +161,27 @@ class App(Music):
     
     def frameScreen(self):
         '''
-            Frames da aplicação
+            Frames da aplicação.\n
+            |\n
+            |\n
+            `--> frame_TopLeft : Icon de música\n
+            `--> frame_BotLeft : Botões de ação\n
+            `--> frame_Right   : Lista de músicas [pasta: 'song']
+	    '''
 
-            Return:
-                frame_TopLeft: Icon de música\n
-                frame_BotLeft: Botões de ação\n
-                frame_Right: Lista de músicas [pasta: 'song']
-        '''
         self.frame_TopLeft = Frame(self.root, bg=bg_frame)
         self.frame_BotLeft = Frame(self.root, bg=bg_screen)
         self.frame_Right = Frame(self.root, bg=bg_screen)
 
     def labelScreen(self):
         '''
-            Labels da aplicação
+            Labels da aplicação.\n
+            |\n
+            |\n
+            `--> set_image : Label da imagem mixer.\n
+            `--> set_music : Label onde será mostrada a música que está tocando.\n
+	    '''
 
-                set_image: Label da imagem mixer.
-                set_music: Label onde será mostrada a música que está tocando.
-        '''
         self.image_Label = Image.open('img/mixer-de-dj.png')
         self.image_Label = self.image_Label.resize((134, 134))
         self.image_Label = ImageTk.PhotoImage(self.image_Label)
@@ -181,21 +196,20 @@ class App(Music):
 
     def gadgetsFrame(self):
         '''
-            Gadgets para aplicar as funções.
+            Gadgets para aplicar as funções.\n
+            |\n
+            |\n
+            `--> listMusic : Lista para armazenar as músicas da pasta 'song'\n
+            `--> scrollBar : Scroll unificada à list box de músicas.\n
+                `--> bottom_Prev : Botão Previous\n
+                `--> bottom_Play : Botão Play\n
+                `--> bottom_Next : Botão Next
+                    `--> `Todos para música.`
+	    '''
 
-            Return:
-                listMusic: Lista para armazenar as músicas da pasta 'song'
-                scrollBar: Scroll unificada à list box de músicas.
-                \n
-                bottom_Prev: Botão Previous
-                bottom_Play: Botão Play
-                bottom_Next: Botão Next
-                    |\n
-                    |\n
-                    [---> Todos para música
-        '''
         self.listMusic = Listbox(self.frame_Right, font=('Arial 8 bold'))
-        self.scrollBar = Scrollbar(self.frame_Right)
+        self.scrollBar = Scrollbar(self.frame_Right, command=self.listMusic.yview)
+        self.listMusic.configure(yscrollcommand=self.scrollBar)
 
         self.image_Prev = Image.open('img/prev.png')
         self.image_Prev = self.image_Prev.resize((34, 34))
@@ -226,8 +240,12 @@ class App(Music):
 
     def position(self):
         '''
-            Posições dos gadgets da aplicação.
+            Posições dos gadgets da aplicação.\n
+                |\n
+                |\n
+                `--> Gadgets.
         '''
+
         # def [frameScreen]
         self.frame_TopLeft.place(relx=0, rely=0, relwidth=0.45, relheight=0.5)
         self.frame_BotLeft.place(relx=0, rely=0.5, relwidth=0.45, relheight=0.5)
@@ -246,21 +264,22 @@ class App(Music):
         
 #  ======================================================
 # APP [ativação]
+@group()
+def groupPainel(status):
+    if status == "sucess":
+        yield Panel("Aplicação ativada com sucesso!", style="blue")
+    else:
+        yield Panel("Algo deu errado...", style="red")
+
 try:
     if __name__ == '__main__':
         mixer.init()
         App()
-    letSet = "\033[47m  \033[m"
-    statusSucess = "\033[46m Ativação feita com sucesso! \033[m" #Cyan Background
-    newSetSucess = f"\033[1m{statusSucess}\033[m"
-    print(f"{letSet}{newSetSucess}")
+    print(Panel(groupPainel("sucess")))
         
 
 except AttributeError as e:
-    letSet = "\033[47m [ERROR] \033[m"
-    statusError = f"\033[41m {e} \033[m" #Red Background
-    newSetError = f"\033[1m{statusError}\033[m"
-    print(f"{letSet}{newSetError}")
+    print(Panel(groupPainel("error")))
 
     messagebox.showerror(
         title='[ERROR] - Ativação',
