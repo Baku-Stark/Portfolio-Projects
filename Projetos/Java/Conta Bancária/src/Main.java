50ae81165aba5670be59b992efd8d7b0bdc793aa
Baku Wall-e <wysthellyjessemata13@gmail.com>
2024-05-09 19:34:21 -0300

    ☕ Update : `Conta Bancária - Project End`

--- Projetos/Java/Conta Bancária/src/Main.java
+++ Projetos/Java/Conta Bancária/src/Main.java
@@ -1,6 +1,28 @@
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Informe o número do banco: ");
+		int numero = scanner.nextInt();
+		System.out.println();
+		
+		System.out.println("Valor do saldo R$");
+		double saldo = scanner.nextDouble();
+		System.out.println();
+		
+		scanner.nextLine();
+		System.out.println("Informe o nome da agência\nr: ");
+		String agencia = scanner.nextLine();
+		System.out.println();
+		
+		System.out.println("Nome do cliente\nr: ");
+		String nomeCliente = scanner.nextLine();
+		System.out.println();
+		
+		System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, \nSua agência é: " + agencia + "\nConta: " + numero + "\nSaldo(Cliente) <R$" + saldo + "> já está disponível");
+		
+		scanner.close();
 	}
 }
